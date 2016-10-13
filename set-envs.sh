d201071d7837f9e4a54cbca0c3d4b70a873a6325
Darren Reid <darren.j.reid@gmail.com>
2016-10-13 13:22:41 +1100

    Simplify build deploy scripts

--- set-envs.sh
+++ set-envs.sh
@@ -5,8 +5,6 @@ export IMAGE_NAME=netcoreapps-northwind
 export IMAGE_VERSION=latest
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
-#AWS_ACCOUNT_NUMBER={} set in private variable
-export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
-export ECS_SERVICE=$IMAGE_NAME-service
-export ECS_TASK=$IMAGE_NAME-task
+export AWS_VIRTUAL_HOST=northwind.netcore.io
+export AWS_ECS_HOST_PORT=5001
